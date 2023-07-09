eb6ee5087984a9dd803d42a8021f82c9b2fedf53
7nib <7nirbhay@gmail.com>
2023-07-09 11:09:45 -0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// configrationqq
+// configration
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
