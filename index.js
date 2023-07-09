abbdfdbe8b8e456a4b0616c8c1b1da41f0040168
7nib <7nirbhay@gmail.com>
2023-07-09 11:35:24 -0700

    adding server connections

--- index.js
+++ index.js
@@ -36,3 +36,16 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at ${PORT}`);
+    });
+  }).catch(err => console.log('did error'))
