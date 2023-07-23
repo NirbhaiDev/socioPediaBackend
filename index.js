aa9a6ff44aa8a0046d9ad0a2fb8492aa72803bf7
7nib <7nirbhay@gmail.com>
2023-07-23 15:07:59 -0700

    userDetailRoutes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from './routes/auth.js'
+import userRoutes from './routes/user.js'
 import {register} from './controllers/auth.js'
 
 // configration
@@ -44,6 +45,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // auth  Routes 
 app.use("/auth", authRoutes)
+app.use("/user", userRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
